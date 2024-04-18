bdbc28c7bfbd6cd0cde616b2255be855aaed37c7
wkidit <132723008+wkidit@users.noreply.github.com>
2024-04-18 13:06:48 +0700

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -2,7 +2,7 @@ const BLOG = {
   title: 'tamaGIOtchi',
   author: 'widikidiw',
   email: 'i@wkid.net',
-  link: 'https://nobelium.vercel.app',
+  link: 'https://wkidnews.vercel.app',
   description: 'media focusing on video games, esports, and tech contents.',
   lang: 'en-US', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
   timezone: 'Asia/Shanghai', // Your Notion posts' date will be interpreted as this timezone. See https://en.wikipedia.org/wiki/List_of_tz_database_time_zones for all options.
