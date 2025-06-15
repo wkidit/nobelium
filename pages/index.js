9771a11e32ceab506c2dbf1311dbf6c62cb3321e
wkidit <132723008+wkidit@users.noreply.github.com>
2025-06-15 19:28:21 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
 import { clientConfig } from '@/lib/server/config'
-import Script from 'next/script'
 import Container from '@/components/Container'
 import BlogPost from '@/components/BlogPost'
 import Pagination from '@/components/Pagination'
 import { getAllPosts } from '@/lib/notion'
 import { useConfig } from '@/lib/config'
+import { useEffect } from 'react'
 
 export async function getStaticProps () {
   const posts = await getAllPosts({ includePages: false })
@@ -23,6 +23,13 @@ export async function getStaticProps () {
 
 export default function Blog ({ postsToShow, page, showNext }) {
   const { title, description } = useConfig()
+  useEffect(() => {
+  const script = document.createElement('script')
+  script.src = '//www.highperformanceformat.com/b58e4c3930f31cf14828b39200130126/invoke.js'
+  script.type = 'text/javascript'
+  script.async = true
+  document.getElementById('ad-container')?.appendChild(script)
+}, [])
 
   return (
     <Container title={title} description={description}>
@@ -30,27 +37,7 @@ export default function Blog ({ postsToShow, page, showNext }) {
         <BlogPost key={post.id} post={post} />
       ))}
       {showNext && <Pagination page={page} showNext={showNext} />}
-  {/* Iklan */}
-    <div style={{ marginTop: '2rem', textAlign: 'center' }}>
-      <div id="ad-container" />
-      <Script id="ads-script" strategy="afterInteractive">
-        {`
-          atOptions = {
-            'key' : 'b58e4c3930f31cf14828b39200130126',
-            'format' : 'iframe',
-            'height' : 300,
-            'width' : 160,
-            'params' : {}
-          };
-          (function() {
-            var script = document.createElement('script');
-            script.type = 'text/javascript';
-            script.src = '//www.highperformanceformat.com/b58e4c3930f31cf14828b39200130126/invoke.js';
-            document.getElementById('ad-container').appendChild(script);
-          })();
-        `}
-      </Script>
-    </div>
+  <div id="ad-container" style={{ width: 160, height: 300, margin: '20px auto' }}></div>
     </Container>
   )
 }
