b6f23e5e1bd15e21c8b4060e03a5806f7eb09d95
wkidit <132723008+wkidit@users.noreply.github.com>
2025-06-15 19:12:04 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import { clientConfig } from '@/lib/server/config'
-
+import Script from 'next/script'
 import Container from '@/components/Container'
 import BlogPost from '@/components/BlogPost'
 import Pagination from '@/components/Pagination'
@@ -30,6 +30,27 @@ export default function Blog ({ postsToShow, page, showNext }) {
         <BlogPost key={post.id} post={post} />
       ))}
       {showNext && <Pagination page={page} showNext={showNext} />}
+  {/* Iklan */}
+    <div style={{ marginTop: '2rem', textAlign: 'center' }}>
+      <div id="ad-container" />
+      <Script id="ads-script" strategy="afterInteractive">
+        {`
+          atOptions = {
+            'key' : 'b58e4c3930f31cf14828b39200130126',
+            'format' : 'iframe',
+            'height' : 300,
+            'width' : 160,
+            'params' : {}
+          };
+          (function() {
+            var script = document.createElement('script');
+            script.type = 'text/javascript';
+            script.src = '//www.highperformanceformat.com/b58e4c3930f31cf14828b39200130126/invoke.js';
+            document.getElementById('ad-container').appendChild(script);
+          })();
+        `}
+      </Script>
+    </div>
     </Container>
   )
 }
